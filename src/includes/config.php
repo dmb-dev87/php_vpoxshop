1fd9d3409c61239cf0edb3e3de188fd3760fbd53
dmb-dev87 <dmbdev800@gmail.com>
2020-08-07 10:55:02 -0600

    [TopOneExpert][2020-08-07][change include/config.php]

--- src/includes/config.php
+++ src/includes/config.php
@@ -1,10 +1,11 @@
 <?php
 //$dbcon = mysqli_connect("localhost","root","","vpox") or die('enable connection');
 
-$dbcon = mysqli_connect("localhost","vpoxadmi_sh0p","okcDO)xwrJs0","vpoxadmi_new") or die('enable connection');
+$dbcon = mysqli_connect("localhost","root","","vpoxadmi_new") or die('enable connection');
 //define('BASEURL','https://vpox.to/');
 //define('BASEURL','http://localhost/vpox/');
-define('BASEURL','https://vpox.to/vpox/');
+// define('BASEURL','https://vpox.to/vpox/');
+define('BASEURL','https://localhost:8097/');
 //error_reporting(E_ALL & ~E_NOTICE);
 
 ?>
